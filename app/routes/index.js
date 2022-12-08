d1e1de6ec52908e4bf09a661b8afc52f4333aa86
gianfillipo <gian.angelo@sptech.school>
2022-12-08 11:36:17 -0300

    adicionando rota no index

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,6 +6,7 @@ const userRoute = require('./user.route')
 const machineRoute = require('./machine.route');
 const dataMachineRoute = require('./dataMachine.route');
 const companyRoute = require('./company.route');
+const dashHilaryRoute = require('./dashHilary.route');
 
 const controller = require('../controllers');
 const authorize = require('../middleware/authorize/authorize');
@@ -24,6 +25,8 @@ router.use('/user', userRoute);
 router.use('/machine', authorize.authorize, machineRoute);
 
 router.use('/dataMachine', dataMachineRoute);
+
+router.use('/dashHilary', dashHilaryRoute)
 
 router.use('/company', companyRoute);
 
